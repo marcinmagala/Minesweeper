5aea6889e09af08e25f97ad07384e6c8d4bcb4fd
marcinmagala <marcinmagala@gmail.com>
2022-09-03 02:11:18 +0200

    default setup files

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const board = document.querySelector('.board');
+
+// for (let i = 0; i < 100; i++) {}
+const box = document.createElement('div');
+
+board.addEventListener('click', console.log('jhgg'));
+
+// box.textContent = 1;
+// console.log(box);
+// console.log(box.nodeType === Node.ELEMENT_NODE);
+// console.log(board);
+// board.appendChild(box);
