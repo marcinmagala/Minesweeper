61d2b3a597d29cd64fc9121fd2265b41eb9ddfa7
marcinmagala <marcinmagala@gmail.com>
2022-09-06 16:53:05 +0200

    improve map of neighbours

--- script.js
+++ script.js
@@ -26,7 +26,13 @@ const setMine = function () {
 
 setMine();
 
+const test = function () {
+  console.log('test');
+};
+
 const fieldWithNeighbours = new Map();
+// key === coords from clicked field
+// value === Array(0-8 -> coords of neighbours of such field, 9 -> mine exist (boolean), 10 -> field clicked by user (boolean) )
 
 const buildBoard = function () {
   for (let i = 0; i < boxList.length; i++) {
@@ -39,29 +45,31 @@ const buildBoard = function () {
       box.dataset.coords = `${i}${j}`;
 
       fieldWithNeighbours.set(`${i}${j}`, [
-        [i - 1, j - 1],
-        [i - 1, j],
-        [i - 1, j + 1],
-        [i, j - 1],
-        [i, j + 1],
-        [i + 1, j - 1],
-        [i + 1, j],
-        [i + 1, j + 1],
+        [`${i - 1}${j - 1}`],
+        [`${i - 1}${j}`],
+        [`${i - 1}${j + 1}`],
+        [`${i}${j - 1}`],
+        [`${i}${j + 1}`],
+        [`${i + 1}${j - 1}`],
+        [`${i + 1}${j}`],
+        [`${i + 1}${j + 1}`],
+        false,
         false,
       ]);
 
       for (let l = 0; l < mineCoords.length; l++) {
         if (mineCoords[l][0] === i && mineCoords[l][1] === j) {
           fieldWithNeighbours.set(`${i}${j}`, [
-            [i - 1, j - 1],
-            [i - 1, j],
-            [i - 1, j + 1],
-            [i, j - 1],
-            [i, j + 1],
-            [i + 1, j - 1],
-            [i + 1, j],
-            [i + 1, j + 1],
+            [`${i - 1}${j - 1}`],
+            [`${i - 1}${j}`],
+            [`${i - 1}${j + 1}`],
+            [`${i}${j - 1}`],
+            [`${i}${j + 1}`],
+            [`${i + 1}${j - 1}`],
+            [`${i + 1}${j}`],
+            [`${i + 1}${j + 1}`],
             true,
+            false,
           ]);
         }
       }
@@ -75,6 +83,8 @@ buildBoard();
 
 console.log(fieldWithNeighbours);
 
+// console.log(fieldWithNeighbours.get(`${fieldWithNeighbours.get('11')[1]}`));
+
 // Tworzenie sąsiadów
 
 // Lewy przycisk myszy na dane pole - odsłonięcie pola
@@ -84,6 +94,8 @@ board.addEventListener('click', function (e) {
   // console.log(fieldWithNeighbours.get(`${e.target.dataset.coords}`)[8]);
   if (!fieldWithNeighbours.get(`${e.target.dataset.coords}`)[8]) {
     e.target.style.backgroundColor = 'green';
+    fieldWithNeighbours.set(`${e.target.dataset.coords}`, true);
+    console.log(fieldWithNeighbours);
   } else {
     e.target.style.backgroundColor = 'red';
   }
